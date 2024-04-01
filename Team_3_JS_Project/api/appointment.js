cc27e549b1d468c78fccd8315c404794e85b35da
Leah Yapp <leahyapp@gmail.com>
2024-04-01 14:23:12 -0400

    all appointment fetch api

--- Team_3_JS_Project/api/appointment.js
+++ Team_3_JS_Project/api/appointment.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const client = require('../db');
-
 const appointmentRouter = express.Router();
 appointmentRouter.use(bodyParser.json());
 
 appointmentRouter.get('/getAppointment', async (req, res) => {
   try {
     const appointmentCollection = client.db('clinic').collection('appointment');
-    
-    const appointments = await appointmentCollection.find().toArray();
-    
-    res.json(appointments);
+    let customerId = parseInt(req.query.customerId);
+    const appointments = await appointmentCollection.find({ customerId: customerId }).toArray();
+    res.send(appointments);
   } catch (error) {
     console.error('Error fetching appointment:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -22,7 +20,7 @@ appointmentRouter.get('/getAppointmentTime', async (req, res) => {
   try {
     const appointmentCollection = client.db('clinic').collection('appointment');
 
-    const filter = { apptDate: req.query.apptDate }
+    const filter = { apptDate: req.query.apptDate, serviceName: req.query.serviceName };
     
     const appointments = await appointmentCollection.find(filter).toArray();
     
@@ -35,34 +33,65 @@ appointmentRouter.get('/getAppointmentTime', async (req, res) => {
 
 appointmentRouter.post('/insertAppointment', async (req, res) => {
   try {
-      const appointmentCollection = client.db('clinic').collection('appointment');
+    const appointmentCollection = client.db('clinic').collection('appointment');
+    let lastDoc = await appointmentCollection.find().sort({_id:-1}).limit(1).toArray();
+    const appointmentId = parseInt(lastDoc[0].appointmentId) + 1;
+    const appt = req.body;
+    await appointmentCollection.insertOne({ 
+      appointmentId: appointmentId,
+      serviceName: appt.serviceName,
+      servicePrice: appt.servicePrice,
+      apptDate: appt.apptDate,
+      apptTime: appt.apptTime,
+      createdTimeStamp: new Date().toISOString(),
+      status: appt.status,
+      customerId: appt.customerId,
+      paymentId: appt.paymentId,
+      feedbackCompleted: false
+    });
+    res.status(201).json({ message: 'Appointment created successfully' });
+  } catch (error) {
+    console.error('Error creating appointment:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-      const count = await appointmentCollection.countDocuments();
+appointmentRouter.delete('/deleteAppointment', async (req, res) => {
+  try {
+    const appointmentCollection = client.db('clinic').collection('appointment');
 
-      const maxIdDoc = await appointmentCollection.findOne({}, { projection: { appointmentId: { $slice: [count - 1, 1] } } });
+    let appointmentId = parseInt(req.query.appointmentId);
+    await appointmentCollection.findOneAndDelete({appointmentId: appointmentId}, function (err) { 
+      if (err){ 
+        res.status(500).json({ error: 'Internal server error' });
+      } 
+      else{ 
+        res.status(200).json({ message: 'Appointment was deleted'}); 
+      } 
+    });
+  } catch (error) {
+    console.error('Error deleting appointment:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-      const newId = maxIdDoc.appointmentId + 1;
-    
-      const {serviceName, servicePrice, apptDate, apptTime, createdTimeStamp, status, customerId, 
-        paymentId} = req.body;
-      
-      await appointmentCollection.insertOne({ 
-        appointmentId: newId,
-        serviceName: serviceName,
-        servicePrice:servicePrice,
-        apptDate: apptDate,
-        apptTime: apptTime,
-        createdTimeStamp: createdTimeStamp,
-        status: status,
-        customerId: customerId,
-        paymentId: paymentId
-       });
-      
-      res.status(201).json({ message: 'Appointment created successfully' });
-    } catch (error) {
-      console.error('Error creating appointment:', error);
-      res.status(500).json({ error: 'Internal server error' });
+appointmentRouter.put('/updateAppointment/:appointmentId', async (req, res) => {
+  try {
+    const appointmentCollection = client.db('clinic').collection('appointment');
+
+    let appointmentId = parseInt(req.params.appointmentId);
+    let updatedData = {
+      apptDate: req.body.apptDate,
+      apptTime: req.body.apptTime, 
+      serviceName: req.body.serviceName,
+      servicePrice: req.body.servicePrice
     }
+    await appointmentCollection.findOneAndUpdate({appointmentId: appointmentId}, { $set: updatedData});
+    res.status(204).send({ message: 'Appointment updated successfully' });
+  } catch (error) {
+    console.error('Error creating appointment:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 module.exports = appointmentRouter;
