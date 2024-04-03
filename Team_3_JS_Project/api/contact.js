a7f87888f8beadae9736f9c556c29c4e906cfb5a
Lillian-Asus\Administrator <lillian.yingying.wei@gmail.com>
2024-04-02 20:23:31 -0400

    serversideContactAPI and serversidefeedbackAPI

--- Team_3_JS_Project/api/contact.js
+++ Team_3_JS_Project/api/contact.js
@@ -5,9 +5,31 @@ const client = require('../db');
 const contactRouter = express.Router();
 contactRouter.use(bodyParser.json());
 
+contactRouter.post('/insertContact', async(req, res) => {
+    try{
+        const contactCollection = client.db('clinic').collection('contact');
+        let lastDoc = await contactCollection.find().sort({_id:-1}).limit(1).toArray();
+        const contactId = parseInt(lastDoc[0].contactId) + 1;
+        const contactInfo = req.body;
+        await contactCollection.insertOne({
+            contactId: contactId,
+            firstname: contactInfo.firstname,
+            lastname:contactInfo.lastname,
+            email: contactInfo.email,
+            subject: contactInfo.subject,
+            message: contactInfo.message
+        });
+        res.status(201).json({message:'Submit successfully'});
+    } catch (error){
+        console.error('Submission error:', error);
+        res.status(500).json({error: 'Internal server error'});
+    } 
+});
 contactRouter.post('/insertContact', (req, res) => {
 
-  
 });
 
+
 module.exports = contactRouter;
+
+
--- Team_3_JS_Project/api/feedback.js
+++ Team_3_JS_Project/api/feedback.js
@@ -7,9 +7,26 @@ feedbackRouter.use(bodyParser.json());
 
 
 
-feedbackRouter.post('/', (req, res) => {
-
-  
+feedbackRouter.post('/insertFeedback', async (req, res) => {
+    try {
+        const feedbackCollection = client.db('clinic').collection('feedback');
+        let lastDoc = await feedbackCollection.find().sort({_id:-1}).limit(1).toArray();
+        const feedbackId = parseInt(lastDoc[0].feedbackId) + 1;
+        const feedbackInfo = req.body;
+        await feedbackCollection.insertOne({
+            feedbackId:  feedbackId,
+            serviceName:  feedbackInfo.serviceName,
+            source: feedbackInfo.source,
+            preSchedule:  feedbackInfo.preSchedule,
+            promptReceptionLevel:  feedbackInfo.promptReceptionLevel,
+            instructionClarityLevel:  feedbackInfo.instructionClarityLevel,
+            satisfactionLevel:  feedbackInfo.satisfactionLevel,
+        });
+        res.status(201).json({message: 'Feedback submit successfully'});
+    } catch (error) {
+        console.error('Feedback submission error: ', error);
+        res.status(500).json({error:'Internal server error'});
+    }
 });
 
 module.exports = feedbackRouter;
