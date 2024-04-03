39d6696f1c458aa4f05c65cf7d689b7b49e0a609
Lillian-Asus\Administrator <lillian.yingying.wei@gmail.com>
2024-04-03 13:49:49 -0400

    update contact.js to submit data to database

--- Team_3_JS_Project/src/scripts/contact.js
+++ Team_3_JS_Project/src/scripts/contact.js
@@ -1,6 +1,6 @@
 'use strict';
 
-        // Function to validate form on change
+       // Function to validate form on change
         function validateFormOnChange() {
             var firstName = document.getElementById('firstName').value.trim();
             var lastName = document.getElementById('lastName').value.trim();
@@ -28,3 +28,41 @@
         // Attach onchange event listeners to input fields
         document.getElementById('firstName').addEventListener('input', validateFormOnChange);
         document.getElementById('lastName').addEventListener('input', validateFormOnChange);
+
+
+
+
+
+// create eventListener to submit the contactInfo       
+const submitButton = document.getElementById('submit').addEventListener('click',async function(event){
+    event.preventDefault();
+    
+    const formData = document.getElementById('contactForm') ;
+   
+    const newContactInfo = {
+        firstname:formData.elements.namedItem("firstName").value,
+        lastname: formData.elements.namedItem('lastName').value,
+        email: formData.elements.namedItem('emailadd').value,
+        subject: formData.elements.namedItem('Subject').value,
+        message: formData.elements.namedItem('message').value
+    };
+    console.log(newContactInfo);
+    try{
+        const response = await fetch('/insertContact', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(newContactInfo)
+            
+        });
+
+        const data = await response.json();
+        alert(data.message);
+        document.getElementById('contactForm').reset();
+    } catch(error){
+        console.error('Submission error: ', error);
+        alert('An error occurred.Please try again later.');
+    }  
+});
+
