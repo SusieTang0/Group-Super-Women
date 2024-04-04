4da8745504560ee86d3443de1d79d71c3b5e8de1
Shuting Tang <tangst816@gmail.com>
2024-04-04 13:56:10 -0400

    Add bookingNumber function to change the format of appointmentId

--- Team_3_JS_Project/src/scripts/book-completed.js
+++ Team_3_JS_Project/src/scripts/book-completed.js
@@ -6,10 +6,22 @@ var appointmentId = window.localStorage.getItem("appointmentId");
 
 window.addEventListener("load",()=>{
   document.getElementById("serviceName").innerHTML = appt.serviceName;
-  document.getElementById("appointmentId").innerText = "#"+ appointmentId;
+  document.getElementById("appointmentId").innerText = "#"+ bookingNumber(appointmentId);
   document.getElementById("apptDate").innerText = appt.apptDate;
   document.getElementById("apptTime").innerText = appt.apptTime;
   localStorage.removeItem("apptObj");
 });
 
 
+function bookingNumber(bookingOrder){
+  let newString = (parseInt(bookingOrder, 16) + 1).toString(16).toUpperCase();
+  let result = "";
+  if(newString.length < 6){
+    for(let i=0;i < (6-newString.length);i++){
+      result+="0";
+    }
+  }
+  result += newString;
+  
+  return result;
+}
